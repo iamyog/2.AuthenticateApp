1041ce9ac96599a844701e48a0dbb6ad701ec7fa
yogesh <yogeshisawesome@gmail.com>
2017-02-07 12:48:41 +0530

    uploading auth app

---
+++ application/views/dashboard.php
@@ -0,0 +1,11 @@
+<?php include('partials/header.php'); ?>
+	
+<?php include('partials/msg.php'); ?>
+	
+  <div class="row">
+      <div class="col-md-4 col-md-offset-4 text-center" id="card">
+        <h3>All out Project list will be here!!!</h3>                  
+      </div>
+  </div>
+
+<?php include('partials/footer.php'); ?>
